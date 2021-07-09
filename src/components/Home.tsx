54f7511e599f3576004137fa79425331ebeac53e
SurajGupta07 <bgs071998@gmail.com>
2021-07-09 20:05:07 +0530

    Styles: Responsive home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,7 +10,7 @@ export function Home() {
         <div>
             <Header username={'Anonymous'} score={state.score}/>
             <div className="home__block">
-                <h1>Can You Pass This Farm Quiz?</h1>
+                <h1 className="home__header">Can You Pass This Farm Quiz?</h1>
                 <img src={Farm} alt="Farming" className="home__image" />
                 <p className="home__block__text">Farmers are responsible for the food we all rely on to live, and yet, many of
                     us probably don’t know the first thing about owning a thriving farm. For
@@ -19,7 +19,9 @@ export function Home() {
                     but how about heifer or sow? While there can be many perks to being your own
                     boss in the great outdoors, the job requires a lot of expertise. If you’re up to
                     the challenge, test your farming knowledge with this quiz!</p>
-                <button className="button-small" onClick={() => navigate('/quiz')}>Start</button>
+                <div className="home__button--result">
+                    <button className="button-small" onClick={() => navigate('/quiz')}>Start</button>
+                </div>
             </div>
         </div>
     )
